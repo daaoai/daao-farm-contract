70d2a6d78b84b2b2ea2db1cd28fd59a1312397fa
meetsiraja <meetsiraja@gmail.com>
2025-02-19 12:19:08 +0530

    ft: added deployment script

---
+++ script/DeployFarmFactory.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.6;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DAOFarmFactory} from "../src/DAOFarmFactory.sol";
+
+contract DeployFarmFactoryScript is Script {
+    // Configuration
+    address public CARTEL_TOKEN_ADDRESS = 0x98E0AD23382184338dDcEC0E13685358EF845f30;
+
+    address public FEE_ADDRESS = 0x6F1313f206dB52139EB6892Bfd88aC9Ae36Dc54E;
+
+    function run() public {
+        // Get private key from environment
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy WhitelistNFTLottery
+        DAOFarmFactory farmFactory = new DAOFarmFactory(FEE_ADDRESS, FEE_ADDRESS);
+        console.log("Deployed DAOFarmFactory at:", address(farmFactory));
+
+        vm.stopBroadcast();
+    }
+}
+
+// forge script script/DeployFarmFactory.s.sol:DeployFarmFactoryScript \
+//   --rpc-url $RPC_URL \
+//   --private-key $PRIVATE_KEY \
+//   --broadcast \
+//   --verify \
+//   --verifier blockscout \
+//   --verifier-url https://explorer.mode.network/api/
